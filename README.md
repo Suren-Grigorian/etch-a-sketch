b96ee87e4a2c743288b497ac318aeba55d6417f6
SurenGrigorianPC <surenwwe@hotmail.com>
2026-09-15 12:49:07 +0200

    Refactor the code. Turn code into functions.
    Remove dead code/unused variables. Turn repeated
    parts of code into funcitons. Move some styling
    from script.js into styles.css.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # etch-a-sketch
-A simple Etch-a-Sketch 
+A simple Etch-a-Sketch Website
+
+This web-page generates a grid of cells, 
+hovering over which changes their color.
+There is a button to reset the board and a slider to
+choose the size of the grid. 
--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 const gridWidth = 750
 const gridHeight = 750
 const gridSize = 16
-let colorsList = ["#586ba4", "#324376", "#f5dd90", "#f68e5f", "#f76c5e"]
 
-const page = document.getElementById("page")
 const container = document.getElementById("container")
 container.style.width = `${gridWidth}px`
 container.style.height = `${gridHeight}px`
-container.style.backgroundColor = "blue"
-container.style.display = "flex"
-container.style.flexDirection = "column"
+
+const btnReset = document.getElementById("reset")
+btnReset.addEventListener("click", ()=>{
+    clearBoard()
+})
+
+const slider = document.getElementById("myRange");
+slider.addEventListener("change", function() {
+    document.getElementById("output").textContent = slider.value;
+});
 
 
 function fillRow(n, row){
     for (let i=0; i< n; i++){
         const gridCell = document.createElement("div")
         gridCell.className = "singleCell"
-        gridCell.style.width = `${gridWidth / n}px`
-        gridCell.style.height = `${gridHeight / n}px`
-        gridCell.style.borderWidth = "1px"
-        gridCell.style.borderStyle = "solid"
         row.appendChild(gridCell)
     }
 }
@@ -28,69 +29,55 @@ function fillRow(n, row){
 function fillGrid(n){
     for (let i=0; i<n; i++){
         const row = document.createElement("div")
-        row.style.width = `${gridWidth}px`
-        row.style.height = `${gridHeight / n}px`
-        row.style.backgroundColor = "green"
-        row.style.display = "flex"
+        row.className = "row"
         fillRow(n, row)
         container.appendChild(row)
     }
 }
 
-const btnReset = document.getElementById("reset")
-btnReset.addEventListener("click", ()=>{
-    clearBoard()
-})
 
 function clearBoard(){
     const permahoverList = document.getElementsByClassName("permahover")
-    var permahoverArray = [...permahoverList]
+    let permahoverArray = [...permahoverList]
     permahoverArray.forEach(cell=> {
         cell.classList.remove("permahover");
     })
 }
 
 
-fillGrid(gridSize)
-const singleCellList = document.querySelectorAll(".singleCell")
-var singleCellArray = [...singleCellList]
-singleCellArray.forEach(cell => {
-    cell.addEventListener("mouseover",()=> {
-        cell.classList.add("permahover")
+function mouseover_listener(){
+    const singleCellList = document.querySelectorAll(".singleCell")
+    const singleCellArray = [...singleCellList]
+    singleCellArray.forEach(cell => {
+        cell.addEventListener("mouseover",()=> {
+            cell.classList.add("permahover")
     })
-})
-
-let userSizeChoice = 16
-var slider = document.getElementById("myRange");
+})}
 
 
-function getSliderValue(){
-    slider.addEventListener("input", (event)=> {
-    console.log(event.target.value)
-    return event.target.value;
-    
-})
+function removeCells(){
+    let singleCellList = document.querySelectorAll(".singleCell")
+        let singleCellArray = [...singleCellList]
+        singleCellArray.forEach(cell => {
+            cell.remove()
+    })
+    let singleRowList = document.querySelectorAll(".row")
+        let singleRowArray = [...singleRowList]
+        singleRowArray.forEach(row => {
+            row.remove()
+    })
 }
 
 
 const btnConfirm = document.getElementById("confirm")
 btnConfirm.addEventListener("click", ()=>{
-    let singleCellList = document.querySelectorAll(".singleCell")
-    var singleCellArray = [...singleCellList]
-    singleCellArray.forEach(cell => {
-        cell.remove()
-    })
-
+    removeCells()
     const userSizeChoice = slider.value
     fillGrid(userSizeChoice)
-    singleCellList = document.querySelectorAll(".singleCell")
-    singleCellArray = [...singleCellList]
-    singleCellArray.forEach(cell => {
-        cell.addEventListener("mouseover",()=> {
-            cell.classList.add("permahover")
-        })
-    })
+    mouseover_listener()
 })
 
 
 
+fillGrid(gridSize)
+mouseover_listener()
